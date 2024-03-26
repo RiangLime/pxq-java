6322fd58d2146ba3ce9b674df61ec7e55a59e44b
jiaqi <jiaqi@queertech.cn>
2024-03-26 16:16:36 +0800

    抢票脚本

--- src/main/java/cn/lime/pxqjava/tool/TicketController.java
+++ src/main/java/cn/lime/pxqjava/tool/TicketController.java
@@ -2,9 +2,11 @@ package cn.lime.pxqjava.tool;
 
 import cn.lime.pxqjava.tool.dto.BizDto;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ClassName: TicketController
@@ -18,8 +20,37 @@ public class TicketController {
     @Resource
     private SendService service;
 
-    @PostMapping("/ticket/start")
-    public String startTicket(@RequestBody BizDto dto){
+    @PostMapping("/ticket/start/post")
+    public String startTicketPost(@RequestBody BizDto dto){
+        service.deal(dto);
+        return "success";
+    }
+
+    @GetMapping("/ticket/start/get")
+    public String startTicketGet(@RequestParam("name") String name,
+                                 @RequestParam(value = "startTime",required = false) String startTime,
+                                 @RequestParam("showId") String showId,
+                                 @RequestParam("sessionId") String sessionId,
+                                 @RequestParam("userSeatId") String userSeatId,
+                                 @RequestParam("buyCount") String buyCount,
+                                 @RequestParam("token") String token,
+                                 @RequestParam("ids") String ids){
+        BizDto dto = new BizDto();
+        dto.setName(name);
+        if (StringUtils.isNotEmpty(startTime)){
+            dto.setStartTime(Long.valueOf(startTime));
+        }
+        dto.setShowId(showId);
+        dto.setSessionId(sessionId);
+        dto.setUserSeatId(userSeatId);
+        dto.setBuyCount(Integer.valueOf(buyCount));
+        dto.setToken(token);
+        List<Integer> audiencesIndexes = new ArrayList<>();
+        for (String s : ids.split(",")) {
+            audiencesIndexes.add(Integer.valueOf(s));
+        }
+        dto.setAudienceIndexes(audiencesIndexes);
+
         service.deal(dto);
         return "success";
     }
